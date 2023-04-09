0cf95ca3328882a79e6f528b251ecc9e8fd8aa40
Paweł Brzozowski <pawel@brzoza.net>
2023-04-09 22:24:00 +0200

    Task #02 (video)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Vimeo.Player(iframe);
+
+player.on("play", function () {
+  console.log("played the video!");
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log("title:", title);
+});
+
+// // //
+
+// const player = new Player("handstick", {
+//   id: 236203659,
+//   width: 640,
+// });
+
+// player.on("play", function () {
+//   console.log("played the video!");
+// });
