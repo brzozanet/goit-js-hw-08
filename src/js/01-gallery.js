77629f8a76c5946ec3ba42d13e8b8e26ac1c310c
Paweł Brzozowski <pawel@brzoza.net>
2023-04-09 19:21:26 +0200

    Task #01 (gallery)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  const imageEl = document.createElement("li");
+  imageEl.classList.add("gallery__item");
+  imageEl.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a>
+    `
+  );
+  galleryEl.append(imageEl);
+}
+
+const imageFromLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 350,
+});
