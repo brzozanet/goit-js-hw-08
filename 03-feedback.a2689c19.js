3de0f8c2307950fb863c4a9121b3e8fbb61cfd31
brzozanet <pawel@brzoza.net>
2023-04-11 10:31:09 +0000

    Deploying to gh-pages from @ brzozanet/goit-js-hw-08@e72964a275e247de6a0197ce58a3ffaf1ebdecfd 🚀

---
+++ 03-feedback.a2689c19.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form");const e=document.querySelector(".feedback-form input"),t=document.querySelector(".feedback-form textarea");let a={email:"",message:""};const o=JSON.parse(localStorage.getItem("feedback-form-state"));console.log(o),e.value=o.email,t.value=o.message;const r=e=>{a[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(a)),console.log(a)};e.addEventListener("input",r),t.addEventListener("input",r)}();
+//# sourceMappingURL=03-feedback.a2689c19.js.map
