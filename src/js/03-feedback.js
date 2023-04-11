a0748b15e36b26acd7f460d71ac70ec05abf7a50
Paweł Brzozowski <pawel@brzoza.net>
2023-04-11 12:09:32 +0200

    Web storage functionality

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+// import { throttle } from "lodash";
+
+const formEl = document.querySelector(".feedback-form");
+const emailEl = document.querySelector(".feedback-form input");
+const messageEl = document.querySelector(".feedback-form textarea");
+
+let userData = {
+    email: "",
+    message: "",
+};
+
+const savedUserData = JSON.parse(localStorage.getItem("feedback-form-state"));
+console.log(savedUserData);
+
+emailEl.value = savedUserData.email;
+messageEl.value = savedUserData.message;
+
+const captureUserData = (event) => {
+userData[event.target.name] = event.target.value;
+console.log(userData);
+localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+};
+
+
+emailEl.addEventListener("input", captureUserData);
+messageEl.addEventListener("input", captureUserData);
 
